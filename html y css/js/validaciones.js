25344c2daa98fae71234b0e352ba6200afd3c029
MartRomeros <mart.romeros@duocuc.cl>
2024-05-02 15:30:54 -0400

    cambios de colores y carrito

--- html y css/js/validaciones.js
+++ html y css/js/validaciones.js
@@ -1,10 +1,10 @@
 function validarCampos(idCampo) {
-    let campos = document.querySelectorAll("#"+idCampo);
+    let campos = document.querySelectorAll("#" + idCampo);
 
     for (let i = 0; i < campos.length; i++) {
         const campo = campos[i];
-        if(campo.value == ""){
-            document.querySelector(".campos").style.border = "red 2px solid";
+        if (campo.value == "") {
+            document.querySelector(".campos").style.border = "#F3414D 2px solid";
             document.querySelector(".error").innerHTML = "<p class='error'>Un campo debe de esta vacio!</p>";
             return false;
         }

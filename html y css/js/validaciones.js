898f3e8fd246b7608eb60b5b6bebf3058ee07314
MartRomeros <mart.romeros@duocuc.cl>
2024-05-09 16:49:28 -0400

    apis de monedas casi listas

--- html y css/js/validaciones.js
+++ html y css/js/validaciones.js
@@ -18,9 +18,42 @@ function validarCampos(nombreCampo) {
     return true;
 }
 
-function agregarProducto() {
-    let cantidad = parseInt(document.querySelector(".cantidad").innerText);
-    cantidad ++;
-    document.querySelector(".cantidad").innerText = cantidad ;
-}
 
+function convertirMonedas() {
+    let precioConvertido = document.querySelector("#precio-conversion").innerText;
+    let opcion = document.querySelector("#destino");
+    let opcionEscogida = opcion.value;
+    fetch('https://mindicador.cl/api').then(function (response) {
+        return response.json();
+    }).then(function (valor) {
+        //document.getElementById("UF").innerHTML = 'El valor actual de la UF es $' + dailyIndicators.uf.valor;
+        switch (opcionEscogida) {
+            case "CLP":
+                document.querySelector("#precio-conversion").innerText = precioConvertido;
+                document.querySelector("#moneda-conversion").innerText = "CLP";
+                break;
+            case "dolar":
+                precioConvertido = parseInt(precioConvertido / valor.dolar.valor);
+                document.querySelector("#precio-conversion").innerText = precioConvertido.toFixed(2);
+                document.querySelector("#moneda-conversion").innerText = "US";
+                break;
+            case "UF":
+                document.querySelector("#precio-conversion").innerText = precioConvertido;
+                document.querySelector("#moneda-conversion").innerText = "UF";
+                break;
+            case "UTM":
+                document.querySelector("#precio-conversion").innerText = precioConvertido;
+                document.querySelector("#moneda-conversion").innerText = "UTM";
+                break;
+            case "Euro":
+                document.querySelector("#precio-conversion").innerText = precioConvertido;
+                document.querySelector("#moneda-conversion").innerText = "Euro";
+                break;
+
+            default:
+                break;
+        }
+    }).catch(function (error) {
+        console.log('Requestfailed', error);
+    });
+}

d05d301349c0e951c36ee06f5ad541cfb9ce0260
MartRomeros <mart.romeros@duocuc.cl>
2024-05-21 21:31:35 -0400

    mejorando el login

--- html y css/js/login.js
+++ html y css/js/login.js
@@ -1,33 +1,3 @@
-//funciones:
-//funcion para validar formularios
-function validarCampos(nombreCampo) {
-    let campos = document.querySelectorAll("." + nombreCampo);
-    for (let i = 0; i < campos.length; i++) {
-        if (campos[i].value == "") {
-            alert("Verifica los campos");
-            document.querySelector("#alerta").style.color = "red";
-            campos[i].style.border = "1px red solid";
-            document.querySelector("#alerta").innerText = "Verifica los campos!";
-            return false;
-
-        } else {
-            document.querySelector("#alerta").style.color = "black";
-            document.querySelector("#alerta").innerText = "";
-            campos[i].style.border = "rgb(163, 163, 163) 1px solid";
-        }
-
-    }
-    return true;
-}
-
-//listeners
-document.querySelector("form").addEventListener("submit", function (evento) {
-    let campo = document.querySelector("input");
-    let nombreClase = campo.className;
-    if (!validarCampos(nombreClase)) {
-        evento.preventDefault();
-    }
-});
 
 
 

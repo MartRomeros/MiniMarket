f3bc7270c1646682dadff612b5732e14c64d8662
MartRomeros <mart.romeros@duocuc.cl>
2024-05-30 15:56:16 -0400

    empezar a registrar productos

--- Market/core/models.py
+++ Market/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=4,primary_key=True)
+    descripcion = models.CharField(max_length=200)
+    stock = models.IntegerField()
+    precio = models.IntegerField()
+    oferta = models.BooleanField()
+    image = models.CharField(max_length=2555)
+
+    
+    
